5f74cca9f0f601c1c655e170fd73d5c2404b4228
SolarisCo <Stathum78@Yahoo.com>
2024-03-20 16:38:15 -0400

    Updated index.php

--- Website/index.php
+++ Website/index.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
     
     // Email configuration
-    $to = 'kahresestathum01@gmail.com'; // Enter your email address here
+    $to = $_SERVER['EMAIL_ADDRESS'];
     $subject = 'New message from your website';
     $body = "Name: $name\nEmail: $email\nMessage:\n$message";
 
